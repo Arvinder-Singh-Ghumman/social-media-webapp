6ded9cf47b3560710278b2900b8ca227567b50b8
Arvinder-Singh-Ghumman <arvindersinghghumman@gmail.com>
2023-03-10 00:28:27 +0530

    check

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,7 +6,7 @@ export const verifyToken = async (req, res, next) => {
     let token = req.header("Authorization");
 
     if(!token) {
-      return res.status(403).send("Access Denied");
+      return res.status(403).send("Access Denied To you");
     }
 
     if(token.startsWith("Bearer ")) {
